373365e24189672a8559a216d544a1f0f3f5af91
Steve Kondik <steve@cyngn.com>
2012-07-20 17:29:34 +0100

    asoc: msm: Always send correct bitwidth to backend
     * Do it during gapless transitions too.
    
    Change-Id: I6d4fca21fc61d21dacf908f46f2ac6abd2eb0450

--- sound/soc/msm/qdsp6v2/msm-compress-q6-v2.c
+++ sound/soc/msm/qdsp6v2/msm-compress-q6-v2.c
@@ -120,6 +120,8 @@ struct msm_compr_audio {
 
 	uint16_t session_id;
 
+	uint16_t bits_per_sample;
+
 	uint32_t sample_rate;
 	uint32_t num_channels;
 
@@ -698,6 +700,7 @@ static int msm_compr_configure_dsp(struct snd_compr_stream *cstream)
 				SNDRV_PCM_STREAM_PLAYBACK,
 				prtd->compr_passthr);
 	} else {
+	    prtd->bits_per_sample = bits_per_sample;
 		pr_debug("%s: stream_id %d\n", __func__, ac->stream_id);
 		ret = q6asm_stream_open_write_v2(ac,
 				prtd->codec, bits_per_sample,
@@ -1604,7 +1607,7 @@ static int msm_compr_trigger(struct snd_compr_stream *cstream, int cmd)
 		}
 		pr_info("%s: open_write stream_id %d", __func__, stream_id);
 		rc = q6asm_stream_open_write_v2(prtd->audio_client,
-				prtd->codec, 16,
+				prtd->codec, prtd->bits_per_sample,
 				stream_id,
 				prtd->gapless_state.use_dsp_gapless_mode);
 		if (rc < 0) {
